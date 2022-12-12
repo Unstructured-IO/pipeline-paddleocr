2aa0ab39abf798b6c0ace542e31cd63280faaaf4
Antonio Jimeno Yepes <antonio@unstructured.io>
2022-12-13 10:51:35 +1100

    API test

--- test_paddleocr/api/test_paddleocr.py
+++ test_paddleocr/api/test_paddleocr.py
@@ -9,3 +9,13 @@ def test_api_health_check():
     response = client.get("/healthcheck")
 
     assert response.status_code == 200
+
+
+def test_api_call():
+    client = TestClient(app)
+    with open("img/0.png", "rb") as f:
+        response = client.post(
+            "/paddleocr/v0.0.1/paddleocr", files={"files": ("filename", f, "image/jpeg")}
+        )
+
+    assert response.status_code == 200
